ee1aab39bc846f542e4914b5f5df02b0f7b58b4a
Tan-Sam <samjoyzful@gmail.com>
2017-08-01 02:27:57 +0800

    Fixed navbar vs content overlap css issue.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ import {
 
 // import { LinkContainer } from 'react-router-bootstrap';
 // import { Route, RouteHandler, Link } from 'react-router';
-// import './NavBar.css';
+import './NavBar.css';
 
 const NavbarInstance = () => (
   <Navbar inverse fixedTop>
@@ -19,9 +19,7 @@ const NavbarInstance = () => (
     <Navbar.Collapse>
       <Nav>
        <NavItem eventKey={1} href="/locations">Places to visit</NavItem>
-       <NavItem eventKey={2} href="/locations">Budget</NavItem>
-        {/*<li><a href='/locations'>Places to visit</a></li>
-        <li><a href='/locations'>Budget</a></li>*/}
+       <NavItem eventKey={2} href="/trip_selection">Select trip</NavItem>
         <NavDropdown eventKey={3} title="Options" id="basic-nav-dropdown">
           <MenuItem eventKey={3.1}>Action</MenuItem>
           <MenuItem eventKey={3.2}>Another action</MenuItem>
@@ -30,8 +28,8 @@ const NavbarInstance = () => (
           <MenuItem eventKey={3.3}>Separated link</MenuItem>
         </NavDropdown>
         <Nav pullRight>
-          <li><a href='/signUp'>Sign Up</a></li>
-          <li><a href='/login'>Log In</a></li>
+          <NavItem eventKey={1} href="/signUp">Sign Up</NavItem>
+          <NavItem eventKey={2} href="/login">Log In</NavItem>
         </Nav>
       </Nav>
     </Navbar.Collapse>
