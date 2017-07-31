130a0cdaf1494e13410381464a9edb89211c48d2
Tan-Sam <samjoyzful@gmail.com>
2017-07-31 22:30:52 +0800

    resolve conflict

--- src/components/UserTripSelection/UserTripSelection.js
+++ src/components/UserTripSelection/UserTripSelection.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import createClass from 'create-react-class';
-import PropTypes from 'prop-types';
-=======
 import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux';
 // import TodoListView from '../TodoListView/TodoListView';
 
 import createClass from 'create-react-class';
->>>>>>> d5ca35b4ea5a331fac5b7dd0476e3aa239b65e1e
 import Select from 'react-select';
 
 import {connect} from 'react-redux';
@@ -17,7 +11,6 @@ import {BUDGET, CATEGORY} from './categoryAndBudget';
 
 import './UserTripSelection.css';
 
-<<<<<<< HEAD
 const UserTripSelections = createClass({
 	displayName: 'UserTripSelections',
 	propTypes: {
@@ -64,123 +57,3 @@ const UserTripSelections = createClass({
 	}
 });
 export default UserTripSelections;
-=======
-
-  const CATEGORY = [
-     {label: "Nightlife", value: "nightlife"},
-     {label: "Tourism", value: "tourism"},
-     {label: "Gaming", value: "gaming"},
-     {label: "Outdoors", value: "outdoors"},
-     {label: "Sightseeing", value: "sightseeing"},
-     {label: "Arts/Entertainment", value: "arts_entertainment"},
-     {label: "Sports", value: "sports"},
-     {label: "Shopping", value: "shopping"},
-     {label: "Animals", value: "animals"},
-     {label: "Tech/Science", value: "tech_science"},
-     {label: "Exploration", value: "exploration"},
-     {label: "Religious Places", value: "religious_places"},
-     {label: "Historical", value: "historical"},
-     {label: "Museum", value: "museum"},
-     {label: "Culture", value: "culture"},
-     {label: "Romantic", value: "romantic"},
-     {label: "Group Activities", value: "group_activities"},
-     {label: "Family-Friendly", value: "family_friendly"},
-     {label: "Water/Beach Activities", value: "water_beach_activities"}
-    ];
-
-  const BUDGET = [
-  	{ label: 'Low ($0 - $30)', value: 'low' },
-  	{ label: 'Medium ($30.01 - $100)', value: 'medium' },
-  	{ label: 'High ($100.01 and above)', value: 'high' }
-  ];
-
-
-  render() {
-    const { tripselection } = this.props;
-
-    const renderTripSelection = () => {
-      if(onClick(className="select-category")) {
-        return (
-          <div>What are you interested in doing?</div>
-        )
-      }
-      elseif(onClick(className="select-budget")) {
-        return (
-          <div>How much is your budget per person?</div>
-        )
-      }
-      else {
-
-
-      }
-
-//map takes the todos in the state (array) & maps it
-      // return tripselection.map( (tripselection) => {
-      //   console.log(tripselection)
-      //   return (
-      //     <TodoListView todo={todo} />
-      //
-      // });
-    }
-
-    return (
-      <div>
-        <div className="messageDiv">
-          <img src="images/1.png" alt="Choose an option for where you want to go."/>
-          <p>Hi! Choose an option for where you want to go.</p>
-        </div>
-        <div className="selectionDiv">
-          <button className="btn btn-success select-category">By Category</button>
-          <button className="btn btn-info select-budget">By Budget  </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-
-// var UserTripSelection = createClass({
-// 	displayName: 'What are you interested in doing?',
-// 	propTypes: {
-// 		label: PropTypes.string,
-// 	},
-// 	getInitialState () {
-// 		return {
-// 			disabled: false,
-// 			options: CATEGORY,
-// 			value: [],
-// 		};
-// 	},
-// 	handleSelectChange (value) {
-// 		console.log("You've selected:", value);
-// 		this.setState({ value });
-// 	},
-// 	toggleDisabled (e) {
-// 		this.setState({ disabled: e.target.checked });
-// 	},
-// 	render () {
-// 		return (
-// 			<div className="section">
-// 				<h3 className="section-heading">{this.props.label}</h3>
-// 				<Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select your interests" options={this.state.options} onChange={this.handleSelectChange} />
-//
-// 				<div className="checkbox-list">
-// 					<label className="checkbox">
-// 						<input type="checkbox" className="checkbox-control" checked={this.state.disabled} onChange={this.toggleDisabled} />
-// 						<span className="checkbox-label">Disable the control</span>
-// 					</label>
-// 				</div>
-// 			</div>
-// 		);
-// 	}
-// });
-// module.exports = UserTripSelection;
-
-export default connect(mapStateToProps, mapDispatchToProps) (UserTripSelection);
-
-// export default connect(
-//     (state) => {
-//       return state;
-//     }
-// ) (UserTripSelection);
->>>>>>> d5ca35b4ea5a331fac5b7dd0476e3aa239b65e1e
