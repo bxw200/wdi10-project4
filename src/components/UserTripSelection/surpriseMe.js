9a4e522e6ec44e3f6668c7f2c8053bb5ee767fb2
Your Name <you@example.com>
2017-08-02 18:47:23 +0800

    CSS for location

--- src/components/UserTripSelection/surpriseMe.js
+++ src/components/UserTripSelection/surpriseMe.js
@@ -8,26 +8,14 @@ import { Link } from 'react-router-dom';
 import {
   Button, Navbar, NavItem, Nav, Grid, Image, Row, Col
 } from 'react-bootstrap';
-
-import {BUDGET, CATEGORY} from './categoryAndBudget';
-import './ResultPreview.css';
-import Location from '../Location/Location';
-import data from '../../data/data';
+import './SurpriseMe.css';
 
 export default class ResultPreview extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-			disabled: false,
-			crazy: false,
-			options: CATEGORY,
-			value: [],
-			sentToServer: true
-		};
-
+    this.state = {place: [] };
+    // this.handleClick = this.handleClick.bind(this);
   }
-
 
   selectAgain = (e) => {
 
@@ -36,30 +24,92 @@ export default class ResultPreview extends React.Component {
 
   surpriseMe = (e) => {
 
-    console.log('clicked: Surprise me!');
-
+    console.log('clicked: Surprise Me!');
   }
+  // var NewItinerary= React.createClass({
 
   confirmTrip= (e) => {
 
+    const user = {
+      name: "john smith",
+      email: "john1234smith@me.org",
+      password: "0000",
+
+      trips: [
+          {
+          duration: 1,
+          pax: 4,
+          budget: 500,
+            itineraries: [
+              {
+                places: [
+                  {
+                    id: 26,
+                    name: "National Museum of Singapore",
+                    address: null,
+                    lat: "1.2967",
+                    lng: "103.8486",
+                    price_pax: 500,
+                    duration: 1
+
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+
+
+
+  //     const toggle = this.state.completed ? false : true;
       console.log('clicked: Confirm trip');
+      // var name = this.state.place.name.value;
+      // var duration = this.state.place.duration.value;
 
+      axios.post('/users', user).
+      then(res => {
+        console.log('Trip confirmed, new trip confirmation worked!', res);
+      }).catch( err => {
+        console.log('error in creating trip', err);
+      });
   }
-
+  // getInitialState() {
+  //   return { places: [] }
+  // }
+  // componentDidMount() { this.props.getJSON('/api/place.json', (place) => {
+  //   this.setState({ places: place })
+  //   });
+  // }
   render() {
-
-    const dispSurprise = this.state.sentToServer? (<div className="randomSurprise">
-			<Location location={data[Math.floor(Math.random()*3)+1]}/>
-			<Location location={data[Math.floor(Math.random()*3)+1]}/>
-			<Location location={data[Math.floor(Math.random()*3)+1]}/>
-		</div>):"";
-
-
+    var placeresult = this.state.place.map((place) => {
+      return (
+        <div key={place.id}>
+          <h3>{place.name}</h3>
+          <p>{place.address}</p>
+        </div> ) });
     return (
       <div>
-
-        {dispSurprise}
-
+        <Row className="show-grid">
+          <Col xs={4} md={4}>
+              <div className="result">
+                <h2>Result 1</h2>
+                {placeresult}
+              </div>
+          </Col>
+          <Col xs={4} md={4}>
+              <div className="result">
+                <h2>Result 2</h2>
+                {placeresult}
+              </div>
+          </Col>
+          <Col xs={4} md={4}>
+              <div className="result">
+                <h2>Result 3</h2>
+                {placeresult}
+              </div>
+          </Col>
+        </Row>
         <Row className="resultchoices">
           <Col xs={2} md={2}>
           <Link
@@ -74,7 +124,7 @@ export default class ResultPreview extends React.Component {
           <Link
           className="btn btn-info"
           role="button"
-          to="/surpriseMe"
+          to="/surpriseme"
           onClick={this.surpriseMe()}>Surprise me!
           </Link>
           </Col>
@@ -83,7 +133,7 @@ export default class ResultPreview extends React.Component {
           <Link
           className="btn btn-info"
           role="button"
-          to="/itinerary"
+          to="/profile"
           onClick={this.confirmTrip()}>Confirm your trip!
           </Link>
           </Col>
